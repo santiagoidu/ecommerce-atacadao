75663bbc910d0de3b95f3aa19da0752b08b6cf39
santiagoidu <sr_santiago@icloud.com>
2021-03-06 02:32:06 -0300

    full ecommerce descontinuado deixado como um V2

--- src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
 import SignInForm from '../../components/sign-in/sign-in.component'
 import SignUpForm from '../../components/sign-up/sign-up.component'
+
+import '../../../src/assets/css/responsive.css'
+
 export default function SignInAndSignUpPage() {
     return (
         <div className="sign-in-and-sign-up">
-            <SignInForm />
-            <SignUpForm />
+            <SignInForm className="singIn"/>
+            <SignUpForm className="singOut"/>
         </div>
     )
 }
